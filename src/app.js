5a62bf071533b8a47a09e9288bc91e45fa0e9a67
Francisco Scagliarini <fscagliarini@incucai.gov.ar>
2025-06-25 15:42:17 -0300

    upload home

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import { config } from "./config/config.js"
+import { homeRouter } from "./router/home.router.js"
 import { fotoRouter } from "./router/fotos.router.js"
 import { accountRouter } from "./router/account.router.js"
 import mongoConectionInstance from "./db/mongo.connection.js"
@@ -9,11 +10,12 @@ await mongoConectionInstance.connect()
 const app = express()
 app.use(express.json())
 
+app.use("", homeRouter)
 app.use("/fotos", fotoRouter)
 app.use("/account", accountRouter)
 
 app.listen(config.PORT, () => {
-    console.log(` -- Armando Mates corriendo en la direccion : http://${config.HOST}:${config.PORT}/ `)
+    console.log(` -- Armando Mates corriendo en RENDER !`)
 })
 
 
---
+++ src/router/home.router.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const homeRouter = Router()
+
+homeRouter.get("", (req, res) => {
+    return req.status(200).json({
+        message: "Bienvenido a la API de Armando Mates. Funcionando en node.js y Render",
+    })
+})
+
+export { homeRouter}
